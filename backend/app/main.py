d0c631dc0b8a6ea63aa37ba00746449f824bf19b
YasKan29 <yashkandepu@gmail.com>
2026-06-13 20:38:45 +0530

    fix CORS syntax error

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,10 +15,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://subnetmagic.vercel.app"
-]
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://subnetmagic.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
